48f16f901662ef45c36ccf1ee35c9b7c5d817f03
Koen Serry <koen@serry.org>
2020-05-07 12:41:35 +0200

    Added test method for nodejs

--- js/webhook.js
+++ js/webhook.js
@@ -8,13 +8,21 @@ const crypto = require('crypto');
  */
 const checkHmacValidity = (apikey,signature, qs) => {
 
-    if (!secret || !qs || !signature) {
+    if (!qs || !signature) {
         throw "Invalid signature or missing api key";
     }
+    let decodedQuerystring = decodeURIComponent(qs);
 
-    let hash = crypto.createHmac('sha256', apikey).update(qs).digest('hex').toUpper();
+    let hash = crypto.createHmac('sha256', apikey).update(decodedQuerystring).digest('hex').toUpperCase();
     // validate and return
-    return hash === hmac
+    return hash === signature
 };
+
+// var test = function(){
+//     const querystring = /*http://my.company.com/webhook?*/ 'msg=dummytest&type=event';
+//     const header_x_signature = '417745C0DE5DE5BFEAF.....'; // header coming from Twikey
+//     const api_key = 'A03EB2.....'; // Api found in your Twikey dashboard
+//     console.log("Valid ? ",checkHmacValidity(api_key,header_x_signature,querystring));
+// }
 
 module.exports = checkHmacValidity;
